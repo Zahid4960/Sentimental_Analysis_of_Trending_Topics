a5608d6a0b5542a0fda5726a47693a2f997d0cf6
Zahid4960 <zahidcse4960@gmail.com>
2019-04-07 22:40:05 +0600

    Added login and logout functionality

--- Sentiment/views.py
+++ Sentiment/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse # import library for http response
 
 # http request for home page
@@ -7,10 +7,12 @@ def home(request):
 	return render(request, 'Sentiment/home.html', { 'title': 'Home' })
 
 # http request for trending page
+@login_required
 def trending(request):
 	return render(request, 'Sentiment/trending.html', { 'title': 'Trending'})
 
 # http request for Check Random page
+@login_required
 def checkRandom(request):
 	return render(request, 'Sentiment/checkRandom.html', { 'title': 'Check Random'})
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
+# for user registration
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -14,3 +16,9 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+# for user profile & user must need to be logged in
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
